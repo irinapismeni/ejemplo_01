46a2506f798ea87b88c7ccfe7e70dbb5ce567578
Irina <irinapismeni@gmail.com>
2022-02-03 18:21:46 -0300

    aber si anda

---
+++ src/main/java/entidades/Perro.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author Irina
+ */
+public class Perro {
+    private String nombre;
+    private String raza;
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+
+
+package main;
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Scanner leer= new Scanner(System.in);
+            
+    }
+
+}
